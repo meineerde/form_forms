d178bec084862dfa7f17d2ae4faf39b22c6c94de
Holger Just <hjust@meine-er.de>
2012-04-08 21:05:09 +0200

    Add basic form test

---
+++ test/forms/form_test.rb
@@ -0,0 +1,13 @@
+module FormForms
+  module Forms
+    class FormTest < ActionView::TestCase
+      test "register form" do
+        assert_nil FormForms::FormRegistry['form_test']
+
+        form = Form.register('form_test'){}
+
+        assert_equal FormForms::FormRegistry['form_test'], form
+      end
+    end
+  end
+end
